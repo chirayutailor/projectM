252fc07267551a2bd2e61b1534956e43d9fc8ad4
vipul550 <hivipulhere203@gmail.com>
2018-06-29 15:19:54 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ReadProductsComponent } from './read-products/read-products.component';
+import { HttpModule } from '@angular/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ReadProductsComponent
+  ],
+  imports: [
+    BrowserModule,
+	HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/product.service.ts
@@ -0,0 +1,26 @@
+import { Subject, ReplaySubject, from, of, range } from 'rxjs';
+import { map, filter, switchMap } from 'rxjs/operators';
+
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+ import 'rxjs/add/operator/map';
+ import 'rxjs/add/operator/catch';
+import { Product } from './product';
+ 
+@Injectable()
+ 
+// Service for products data.
+export class ProductService {
+ 
+    // We need Http to talk to a remote server.
+    constructor(private _http : Http){ }
+ 
+    // Get list of products from remote server.
+    readProducts(): Observable<Product[]>{
+        return this._http
+            .get("http://sonal.lcl/product/read.php")
+            .map(res => res.json());
+    }
+ 
+}
